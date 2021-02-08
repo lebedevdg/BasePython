ab3b3a9eda1f2623c5318aed2eac05a13ab27ccb
Denis Lebedev <d@lebedeva.net>
2021-02-09 01:54:12 +0300

    john

--- Git-shell/home_work.py
+++ Git-shell/home_work.py
@@ -1,32 +1,15 @@
-    def time_call_any(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            print(args)
-            print(kwargs)
-            start = time()
-            res = func(*args, **kwargs)
-            end = time()
-            print("res", func, " = ", res)
-            time_taken = end - start
-            print(f"time taken: {time_taken:.13f}")
-            return res
+    def greet_names(names):
+        # never change in place!!
+        # for name in names:
+        #     print("Hello", name.title())
+        while names:
+            name = names.pop()
+            print("Hello", name.title())
 
-        # wrapper.__wrapped__ = func
-        # wrapper.__name__ = func.__name__
-        return wrapper
+        # return None
 
 
-    @time_call_any
-    def add(a, b):
-        return a + b
-
-    add(17576547654654665, 1757654765465466517576547654654665)
-
-    @time_call_any
-    def hello_again(name="World"):
-        return "Hello " + name
-
-    print("d", hello_again())
-    print("d", hello_again("John"))
-    print("d", hello_again(name="Sam"))
-
+    names = ["sam", "ann", "nick", "john"]
+    print("names:", names)
+    greet_names(names)
+    print("names:", names)
